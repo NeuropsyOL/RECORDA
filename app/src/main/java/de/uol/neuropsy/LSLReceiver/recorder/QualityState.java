901a101f7374672100597e121b88078918f201e0
Sarah Blum <blum@hz-ol.de>
2023-11-11 10:51:46 +0100

    Add interface and quality model for monitoring stream quality

---
+++ app/src/main/java/de/uol/neuropsy/LSLReceiver/recorder/QualityState.java
@@ -0,0 +1,7 @@
+package de.uol.neuropsy.LSLReceiver.recorder;
+
+public enum QualityState {
+    OK,
+    NOT_RESPONDING,
+    LAGGY
+}
---
+++ app/src/main/java/de/uol/neuropsy/LSLReceiver/recorder/StreamQualityListener.java
@@ -0,0 +1,2 @@
+package de.uol.neuropsy.LSLReceiver.recorder;public interface StreamQualityListener {
+}
